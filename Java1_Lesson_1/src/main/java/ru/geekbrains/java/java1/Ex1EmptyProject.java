65677681dc6861290cb506f419cde1d4894f3b99
IDA <isakov.danila.79@gmail.com>
2020-11-24 01:28:27 +0200

    merging failed

---
+++ Java1_Lesson_1/src/main/java/ru/geekbrains/java/java1/Ex1EmptyProject.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.java.java1;
+
+public class Ex1EmptyProject {
+
+// 1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
+
+    public static void main(String[] args) {
+
+    }
+
+}
