40adcba8fa1c12c211bd90c0b22ba0a742ac626f
IDA <isakov.danila.79@gmail.com>
2020-11-24 01:39:16 +0200

    Java1. Lesson 8 homework

---
+++ Java1_Lesson_8/src/main/java/ru/geekbrains/java/java1/LessonSession.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.java.java1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LessonSession {
+
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow();
+        MyWindow myWindow2 = new MyWindow();
+    }
+
+
+}
+
+class MyWindow extends JFrame {
+    public MyWindow() {
+        setBounds(500, 500, 400, 300);
+        setTitle("FlowLayoutDemo");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JButton[] jbs = new JButton[10];
+        setLayout(new FlowLayout());
+        for (int i = 0; i < jbs.length; i++) {
+            jbs[i] = new JButton("#" + i);
+            add(jbs[i]);
+        }
+        setVisible(true);
+
+    }
+}
