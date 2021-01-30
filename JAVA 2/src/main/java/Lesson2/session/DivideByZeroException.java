ae4e31cf62b6e9920639c007860fd6514313bc57
IDA <isakov.danila.79@gmail.com>
2021-01-30 19:24:22 +0200

    Merge all Java Core cources in one
    Join and sctructurise java-files. Deleting IDE files.

--- JAVA 2/src/main/java/Lesson2/session/DivideByZeroException.java
+++
@@ -1,8 +0,0 @@
-package Lesson2.session;
-
-public class DivideByZeroException extends ArithmeticException {
-
-    public DivideByZeroException() {
-        super("Произошло деление на ноль!");
-    }
-}
